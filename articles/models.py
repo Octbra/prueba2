ba38b0f34a88602409439e6d1cb5bdb1f538819e
Octavio Bravo <myth_t@hotmail.com>
2021-12-02 10:17:25 -0300

    Correci[on en Models y se agrega el modelo Clientes

--- articles/models.py
+++ articles/models.py
@@ -3,8 +3,19 @@ from django.db import models
 # Create your models here.
 
 class Restaurantes(models.Model):
-    nombre = models.CharField(max_lenght=100)
-    cocina = models.CharField(max_lenght=100)
+    nombre = models.CharField(max_length=50)
+    cocina = models.CharField(max_length=100)
     direccion = models.CharField(max_length=200)
     telefono = models.IntegerField()
-  
+
+class Bares(models.Model):
+    nombre = models.CharField(max_length=100)
+    direccion = models.CharField(max_length=200)
+    telefono = models.IntegerField()
+    
+class Clientes(models.Model):
+    nombre=models.CharField(max_length=50)
+    apellido=models.CharField(max_length=50)
+    es_huesped=models.BooleanField()
+    habitacion=models.IntegerField()
+    numero_telefono=models.IntegerField()
